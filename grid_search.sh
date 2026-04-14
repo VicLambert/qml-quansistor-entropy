8b337921a67deec589fbfae4df8f3ee2c0910dd7
Victor Lambert <victor.lambert@umontreal.ca>
2026-04-14 07:22:22 -0700

    Adding new files for running codes on server

---
+++ grid_search.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=def-boudrea1
+#SBATCH --job-name=grid_search
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=0
+#SBATCH --output=logs/grid_search.out
+#SBATCH --error=logs/grid_search.err
+#SBATCH --time=2-18:00:00
+
+module load python scipy-stack
+source /project/6099921/NDOT/QML-gates-tests/qqe/.venv/bin/activate
+
+export OMP_NUM_THREADS=1 OPENBLAS_NUM_THREADS=1 MKL_NUM_THREADS=1
+
+python -u grid_search_gnn.py --epochs=50 
